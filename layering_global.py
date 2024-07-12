3608330838b33ca3561ed5f5db6f6601756bfe64
Nathan Collier <nathaniel.collier@gmail.com>
2024-07-12 12:48:09 -0400

    moved masking to after computation to avoid problems with nan's

--- layering_global.py
+++ layering_global.py
@@ -8,7 +8,7 @@ from intake_esgf import ESGFCatalog
 D_MAX = 0.1
 
 mpl.rc("font", **{"size": 18})
-model = "CESM2"  # "CNRM-ESM2-1"  # "UKESM1-0-LL"  # "MPI-ESM1-2-LR" # "CESM2"
+model = "CanESM5"  # "CNRM-ESM2-1"  # "UKESM1-0-LL"  # "MPI-ESM1-2-LR" # "CESM2"
 # "GFDL-ESM4" # "ACCESS-ESM1-5"  # "BCC-CSM2-MR"  # "CanESM5"  # "NorESM2-LM" # "MIROC6"
 cat = ESGFCatalog().search(
     experiment_id="historical",
@@ -17,13 +17,12 @@ cat = ESGFCatalog().search(
     source_id=model,
 )
 cat.remove_ensembles()
-ds = cat.to_dataset_dict(ignore_facets="table_id", add_measures=False)
+ds = cat.to_dataset_dict(ignore_facets="table_id", add_measures=True)
 
-# 1. Calculate the long-term mean of the variable
+# Calculate the long-term mean of the variable
 long_term_mean_mrsos = (
     ds["mrsos"].sel(time=slice("2013-01-01", "2015-01-01")).mean(dim="time")
 )
-# long_term_mean_mrsol = ds["mrsol"].isel(depth=0).mean(dim="time")
 long_term_mean_mrsol = (
     ds["mrsol"].sel(time=slice("2013-01-01", "2015-01-01")).mean(dim="time")
 )
@@ -32,19 +31,28 @@ depth = [d for d in long_term_mean_mrsol.dims if "depth" in d][0]
 depth_bnds = long_term_mean_mrsol[depth].attrs["bounds"]
 ndepth = int((long_term_mean_mrsol[depth_bnds][:, 0] < D_MAX).sum().values)
 
-# # fractional sum of just the parts that overlap the top 10cm?
+# Fractional sum of just the parts that overlap the top 10cm
 bnd = long_term_mean_mrsol[depth_bnds].dims[1]
 weights = long_term_mean_mrsol[depth_bnds][:ndepth].clip(0, D_MAX).diff(
     dim=bnd
 ) / long_term_mean_mrsol[depth_bnds][:ndepth].diff(dim=bnd)
-method2 = long_term_mean_mrsol["mrsol"][:ndepth].weighted(weights).sum(dim=depth)
+method2 = (
+    long_term_mean_mrsol["mrsol"][:ndepth]
+    .weighted(weights.fillna(0))
+    .sum(dim=depth, keep_attrs=True)
+)
 
+# Mask out the non-land portions using the land fraction
+long_term_mean_mrsos = xr.where(
+    ds["mrsol"]["sftlf"] > 1e-8, long_term_mean_mrsos, np.nan, keep_attrs=True
+)
+method2 = xr.where(ds["mrsol"]["sftlf"] > 1e-8, method2, np.nan, keep_attrs=True)
+method2.name = "mrsos"
 
 # Calculate the difference between the long-term means
 difference = long_term_mean_mrsos["mrsos"] - method2
 
 # Choose max colorbar value for plot
-# vmax_plot = float(max(method2.max(), long_term_mean_mrsos["mrsos"].max()))
 vmax_plot = float(50)
 
 # Create a figure with three subplots
@@ -60,7 +68,7 @@ axs[0].set_ylabel("Latitude")
 
 # Plot mrsol in the second subplot
 method2.plot(ax=axs[1], vmax=vmax_plot)  # Selecting the DataArray before plotting
-axs[1].set_title("Long-term Mean mrsol")
+axs[1].set_title("Long-term Mean Top 10 cm mrsol")
 axs[1].set_xlabel("Longitude")
 axs[1].set_ylabel("Latitude")
 
