b347a899a2be65ccc91959aec75b83feadb7e2fa
Nathan Collier <nathaniel.collier@gmail.com>
2024-07-12 12:25:58 -0400

    removed commented out code, you will still have it in other files on your computer or in this history

--- layering_global.py
+++ layering_global.py
@@ -1,10 +1,8 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 import xarray as xr
 from intake_esgf import ESGFCatalog
-import numpy as np
-
-# import cartopy.crs as ccrs
 
 # How deep is the layer to integrate
 D_MAX = 0.1
@@ -20,69 +18,6 @@ cat = ESGFCatalog().search(
 )
 cat.remove_ensembles()
 ds = cat.to_dataset_dict(ignore_facets="table_id", add_measures=False)
-# ds.load()
-
-# for key, d in ds.items():
-#     if "mrsos" not in d:
-#         continue
-#     if "depth" in ds[key]:
-#         ds[key] = ds[key].drop_vars("depth")
-#     if "depth_bnds" in d:
-#         ds[key] = ds[key].drop_vars("depth_bnds")
-# ds = [
-#     d.sel(lat=60.0, lon=235.0, method="nearest").isel(time=1807) for _, d in ds.items()
-# ]
-# ds = xr.merge(ds)
-# ds.load()
-# depth = [d for d in ds.dims if "depth" in d][0]
-# depth_bnds = ds[depth].attrs["bounds"]
-# ndepth = int((ds[depth_bnds][:, 0] < D_MAX).sum().values)
-
-# # # fractional sum of just the parts that overlap the top 10cm?
-# bnd = ds[depth_bnds].dims[1]
-# weights = ds[depth_bnds][:ndepth].clip(0, D_MAX).diff(dim=bnd) / ds[depth_bnds][
-#     :ndepth
-# ].diff(dim=bnd)
-# method2 = float(ds["mrsol"][:ndepth].weighted(weights).sum())
-
-
-# fig, ax = plt.subplots(figsize=(6, 10), tight_layout=True)
-# for d in range(len(ds[depth])):
-#     lbl = None if d else "mrsol"
-#     ax.plot(
-#         [ds["mrsol"][d]] * 2,
-#         ds[depth_bnds][d],
-#         "-",
-#         color="tab:blue",
-#         lw=2,
-#         label=lbl,
-#     )
-# ax.grid()
-# ax.set_ylim(0.23, -0.05)
-# ax.set_xlim(0, 160)
-# ax.set_xlabel("Soil Moisture [kg m-2]")
-# ax.set_ylabel("Depth [m]")
-# ax.plot(
-#     [ds["mrsos"]] * 2,
-#     [0, 0.1],
-#     "-",
-#     color="tab:orange",
-#     lw=2,
-#     label=f"mrsos ({ds['mrsos']:.2f})",
-# )
-# ax.plot(
-#     [method2] * 2,
-#     [0, D_MAX],
-#     "--",
-#     color="tab:red",
-#     lw=2,
-#     label=f"method2 ({method2:.2f})",
-# )
-# fig.legend()
-# # fig.savefig(f"{model}.png")
-# # plt.close()
-# plt.show()
-
 
 # 1. Calculate the long-term mean of the variable
 long_term_mean_mrsos = (
@@ -138,41 +73,3 @@ axs[2].set_ylabel("Latitude")
 # Adjust layout
 plt.tight_layout()
 plt.show()
-
-
-# for key, d in ds.items():
-#     if "mrsos" not in d:
-#         continue
-#     if "depth" in ds[key]:
-#         ds[key] = ds[key].drop_vars("depth")
-#     if "depth_bnds" in d:
-#         ds[key] = ds[key].drop_vars("depth_bnds")
-# ds = [
-#     d.sel(lat=60.0, lon=235.0, method="nearest").isel(time=1807) for _, d in ds.items()
-# ]
-# ds = xr.merge(ds)
-# ds.load()
-# depth = [d for d in ds.dims if "depth" in d][0]
-# depth_bnds = ds[depth].attrs["bounds"]
-# ndepth = int((ds[depth_bnds][:, 0] < D_MAX).sum().values)
-
-
-# # fractional sum of just the parts that overlap the top 10cm?
-# bnd = ds[depth_bnds].dims[1]
-# weights = ds[depth_bnds][:ndepth].clip(0, D_MAX).diff(dim=bnd) / ds[depth_bnds][
-#     :ndepth
-# ].diff(dim=bnd)
-# method2 = float(ds["mrsol"][:ndepth].weighted(weights).sum())
-
-
-# ax.plot(
-#     [method2] * 2,
-#     [0, D_MAX],
-#     "--",
-#     color="tab:red",
-#     lw=2,
-#     label=f"method2 ({method2:.2f})",
-# )
-# fig.legend()
-# fig.savefig(f"{model}.png")
-# plt.close()
